921353ca4548d51119545078546cf8391c115cdd
Diego Burgos <d.a.burgos.servellon@gmail.com>
2024-03-08 22:10:23 -0600

    ultima guia, cambio 1, no funciona el GetAll con Joins

---
+++ webApiPractica/Models/tipo_equipo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webApiPractica.Models
+{
+    public class tipo_equipo
+    {
+        [Key]
+        public int id_tipo_equipo { get; set; }
+        public String descripcion { get; set; }
+        public bool estado { get; set; }
+
+    }
+}
